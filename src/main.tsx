e361e17f58f8de54d091f6669b73058be803824a
p3nc1l <david@davidszocs.com>
2025-08-26 18:22:59 +0300

    switched from browser router to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { HashRouter, Routes, Route } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={ <App /> }  />
         <Route path="en" element={ <App lang='en' /> } />
         <Route path="hu" element={ <App lang='hu' /> } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
